a43f4299eea51cfc4a2040a04c55628ef22224d9
chysis <chysiss@naver.com>
2026-05-29 01:12:11 +0900

    setting: Storybook 및 Chromatic 설치 및 빌드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const rootDir = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
@@ -13,13 +17,37 @@ export default defineConfig({
   plugins: [svgr(), react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(rootDir, "src"),
-    },
+      "@": path.resolve(rootDir, "src")
+    }
   },
   test: {
-    environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts"],
-    include: ["src/**/*.{test,spec}.{ts,tsx}"],
-    exclude: ["e2e/**", "node_modules/**", "dist/**"],
-  },
+    projects: [{
+      extends: true,
+      test: {
+        environment: "jsdom",
+        setupFiles: ["./src/test/setup.ts"],
+        include: ["src/**/*.{test,spec}.{ts,tsx}"],
+        exclude: ["e2e/**", "node_modules/**", "dist/**"]
+      }
+    }, {
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
+  }
 });

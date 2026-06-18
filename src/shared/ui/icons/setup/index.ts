aa456e2d2f03af240c36bf9af79a1b7456a7b65e
chysis <chysiss@naver.com>
2026-06-18 21:52:23 +0900

    프로그레스 바 공통 컴포넌트 구현 및 setup 아이콘 추가

---
+++ src/shared/ui/icons/setup/index.ts
@@ -0,0 +1,4 @@
+export { default as SetupCheckIcon } from "./Check.svg?react";
+export { default as SetupMicLargeIcon } from "./MicLarge.svg?react";
+export { default as SetupMicSmallIcon } from "./MicSmall.svg?react";
+export { default as SetupPlaySmallIcon } from "./PlaySmall.svg?react";
